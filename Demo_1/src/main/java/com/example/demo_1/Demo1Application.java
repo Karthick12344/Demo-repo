db79a07d5784d28390812220d1d80e4d63d6ec52
Karthick12344 <71247376+Karthick12344@users.noreply.github.com>
2025-12-22 10:49:23 -0800

    Add files via upload

---
+++ Demo_1/src/main/java/com/example/demo_1/Demo1Application.java
@@ -0,0 +1,26 @@
+package com.example.demo_1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1Application.class, args);
+
+        Logger logger = LoggerFactory.getLogger(Demo1Application.class);
+
+        logger.trace("Log level: TRACE");
+        logger.info("Log level: INFO");
+        logger.debug("Log level: DEBUG");
+        logger.error("Log level: ERROR");
+        logger.warn("Log level: WARN");
+
+        System.out.println("Test");
+
+    }
+
+}
